6d5afe8107b93190c3276e8b34ed924b6601710e
lauirvin <lauirvin98@gmail.com>
2018-12-04 13:00:47 +0000

    frontend cleanup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import NavBar from "./components/NavBar/NavBar";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div>
+          <p>Work in progress.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
